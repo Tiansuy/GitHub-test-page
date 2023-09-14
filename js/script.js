1e18d31f4e0ea980467a87828a9dd3d4786647fa
Tiansuy <2734333845@qq.com>
2023-09-14 18:21:21 -0500

    update

---
+++ js/script.js
@@ -0,0 +1,210 @@
+(function($) {
+	
+	"use strict";
+	//¸ü¶àÏÂÔØ£ºHttp://www.bootstrapmb.com
+	//Hide Loading Box (Preloader)
+	function handlePreloader() {
+		if($('.preloader').length){
+			$('.preloader').delay(200).fadeOut(500);
+		}
+	}
+	
+	//Update Header Style and Scroll to Top
+	function headerStyle() {
+		if($('.main-header').length){
+			var windowpos = $(window).scrollTop();
+			var siteHeader = $('.header-style-one');
+			var scrollLink = $('.scroll-to-top');
+			var sticky_header = $('.main-header .sticky-header');
+			if (windowpos > 100) {
+				sticky_header.addClass("fixed-header animated slideInDown");
+				scrollLink.fadeIn(300);
+			}else {
+				sticky_header.removeClass("fixed-header animated slideInDown");
+				scrollLink.fadeOut(300);
+			}
+			if (windowpos > 1) {
+				siteHeader.addClass("fixed-header");
+			}else {
+				siteHeader.removeClass("fixed-header");
+			}
+		}
+	}
+	headerStyle();
+
+	//Submenu Dropdown Toggle
+	if($('.main-header li.dropdown ul').length){
+		$('.main-header .navigation li.dropdown').append('<div class="dropdown-btn"><i class="fa fa-angle-down"></i></div>');
+	}
+
+	//Mobile Nav Hide Show
+	if($('.mobile-menu').length){
+		
+		var mobileMenuContent = $('.main-header .main-menu .navigation').html();
+		$('.mobile-menu .close-btn').on('click', function() {
+			$('body').removeClass('mobile-menu-visible');
+		});
+		
+		//Dropdown Button
+		$('.mobile-menu li.dropdown .dropdown-btn').on('click', function() {
+			$(this).prev('ul').slideToggle(500);
+			$(this).toggleClass('active');
+		});
+
+		//Menu Toggle Btn
+		$('.mobile-nav-toggler').on('click', function() {
+			$(this).addClass('active');
+			$('body').addClass('mobile-menu-visible');
+		});
+
+		//Menu Toggle Btn
+		$('.mobile-menu .menu-backdrop, .mobile-menu .close-btn').on('click', function() {
+			$('body').removeClass('mobile-menu-visible');
+			$('.mobile-nav-toggler').removeClass('active');
+		});
+	}
+
+	//Header Search
+	if($('.search-btn').length) {
+		$('.search-btn').on('click', function() {
+			$('.main-header').addClass('moblie-search-active');
+		});
+		$('.close-search, .search-back-drop').on('click', function() {
+			$('.main-header').removeClass('moblie-search-active');
+		});
+	}
+
+
+	//Accordion Box
+	if ($('.accordion-box').length) {
+		$(".accordion-box").on('click', '.acc-btn', function () {
+			var outerBox = $(this).parents('.accordion-box');
+			var target = $(this).parents('.accordion');
+			if ($(this).hasClass('active') !== true) {
+				$(outerBox).find('.accordion .acc-btn').removeClass('active ');
+			}
+			if ($(this).next('.acc-content').is(':visible')) {
+				return false;
+			} else {
+				$(this).addClass('active');
+				$(outerBox).children('.accordion').removeClass('active-block');
+				$(outerBox).find('.accordion').children('.acc-content').slideUp(300);
+				target.addClass('active-block');
+				$(this).next('.acc-content').slideDown(300);
+			}
+		});
+	}
+
+
+	//Fact Counter + Text Count
+	if($('.count-box').length){
+		$('.count-box').appear(function(){
+			var $t = $(this),
+				n = $t.find(".count-text").attr("data-stop"),
+				r = parseInt($t.find(".count-text").attr("data-speed"), 10);
+				
+			if (!$t.hasClass("counted")) {
+				$t.addClass("counted");
+				$({
+					countNum: $t.find(".count-text").text()
+				}).animate({
+					countNum: n
+				}, {
+					duration: r,
+					easing: "linear",
+					step: function() {
+						$t.find(".count-text").text(Math.floor(this.countNum));
+					},
+					complete: function() {
+						$t.find(".count-text").text(this.countNum);
+					}
+				});
+			}
+		},{accY: 0});
+	}
+
+
+	//Tabs Box
+	if ($('.tabs-box').length) {
+		$('.tabs-box .tab-buttons .tab-btn').on('click', function (e) {
+			e.preventDefault();
+			var target = $($(this).attr('data-tab'));
+
+			if ($(target).is(':visible')) {
+				return false;
+			} else {
+				target.parents('.tabs-box').find('.tab-buttons').find('.tab-btn').removeClass('active-btn');
+				$(this).addClass('active-btn');
+				target.parents('.tabs-box').find('.tabs-content').find('.tab').fadeOut(0);
+				target.parents('.tabs-box').find('.tabs-content').find('.tab').removeClass('active-tab animated fadeIn');
+				$(target).fadeIn(300);
+				$(target).addClass('active-tab animated fadeIn');
+			}
+		});
+	}
+
+
+	//Progress Bar
+	if ($('.progress-line').length) {
+		$('.progress-line').appear(function () {
+			var el = $(this);
+			var percent = el.data('width');
+			$(el).css('width', percent + '%');
+		}, { accY: 0 });
+	}
+
+	
+	//LightBox / Fancybox
+	if($('.lightbox-image').length) {
+		$('.lightbox-image').fancybox({
+			openEffect  : 'fade',
+			closeEffect : 'fade',
+			helpers : {
+				media : {}
+			}
+		});
+	}
+
+	// Scroll to a Specific Div
+	if($('.scroll-to-target').length){
+		$(".scroll-to-target").on('click', function() {
+			var target = $(this).attr('data-target');
+		   // animate
+		   $('html, body').animate({
+			   scrollTop: $(target).offset().top
+			 }, 0);
+	
+		});
+	}
+	
+	// Elements Animation
+	if($('.wow').length){
+		var wow = new WOW(
+		  {
+			boxClass:     'wow',      // animated element css class (default is wow)
+			animateClass: 'animated', // animation css class (default is animated)
+			offset:       0,          // distance to the element when triggering the animation (default is 0)
+			mobile:       false,       // trigger animations on mobile devices (default is true)
+			live:         true       // act on asynchronously loaded content (default is true)
+		  }
+		);
+		wow.init();
+	}
+	
+/* ==========================================================================
+   When document is Scrollig, do
+   ========================================================================== */
+	
+	$(window).on('scroll', function() {
+		headerStyle();
+	});
+	
+/* ==========================================================================
+   When document is loading, do
+   ========================================================================== */
+	
+	$(window).on('load', function() {
+		handlePreloader();
+	});	
+
+})(window.jQuery);
